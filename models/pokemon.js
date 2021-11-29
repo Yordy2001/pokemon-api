7708d90c496f4aee1144afb797eab99c138b3679
Yordy2001 <kingyordi2001@gmail.com>
2021-11-28 22:05:16 -0400

    add pg-connection, models and sedders

---
+++ models/pokemon.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pokemon extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Pokemon.init({
+    name: DataTypes.STRING,
+    ability: DataTypes.STRING,
+    type: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Pokemon',
+  });
+  return Pokemon;
+};
