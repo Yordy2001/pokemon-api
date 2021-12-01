16e897c4451d6e7593ee6a6e3006f18efdd1b076
Yordy2001 <kingyordi2001@gmail.com>
2021-12-01 10:56:12 -0500

    add seeder

---
+++ db/seeders/20211201154515-yordy.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      name:'yordy',
+      password:'123456',
+      email:'yramirez@gmail.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {})
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {})
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
