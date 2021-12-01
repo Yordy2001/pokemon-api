cc7a79de6b5a64beebdbfc423c2c1bc22615cad0
Yordy2001 <kingyordi2001@gmail.com>
2021-12-01 10:12:46 -0500

    create model User and migrate

--- routers/auth/middleware/authenticate.js
+++ routers/auth/middleware/authenticate.js
@@ -1,35 +1,6 @@
-let auth = false
-
-const dbModel =[
-    {
-        userName:"yordy",
-        password:"12345"
-    },
-
-    {
-        userName:"ramon",
-        password:"1234567"
-    },
-
-    {
-        userName:"oscar",
-        password:"123456"
-    }
-]
 module.exports = (req, res, next ) =>{
-    const {userName, password} = req.body
-    let user = dbModel.includes(userName)
-
-    if(user){
-        auth = true
-    }
     
-    console.log(user)
-    if(auth){
-        next()
+    res.send("middlware correct")
 
-    }else{
-        res.send("user dont has acces to this part")
-    }
+    next()
 }
-
