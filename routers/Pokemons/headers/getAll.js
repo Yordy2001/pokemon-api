9b77a57d131074cf11b7312a897e1075ab16fb6e
Yordy2001 <kingyordi2001@gmail.com>
2022-01-04 16:24:45 -0500

    show user name, add form to add pokemon

--- routers/Pokemons/headers/getAll.js
+++ routers/Pokemons/headers/getAll.js
@@ -3,13 +3,14 @@ const {Pokemon} = require('../../../db');
 
 module.exports = async (req, res) => {
     data = []
+    let user = req.session.user
     try {
         const pokemons = await Pokemon.findAll();
         pokemons.forEach(element => {
             const {name, ability, type} = element
             data = [...data, {name, ability, type}]
         });
-        res.render(path.resolve(__dirname, '../../../static/templates/pokemon.pug'), {data})
+        res.render(path.resolve(__dirname, '../../../static/templates/pokemon.pug'), {data, user})
     } catch (error) {
         console.log(error)
     }
