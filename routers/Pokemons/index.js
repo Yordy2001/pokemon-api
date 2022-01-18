b177b7c3fa6dfa124367152a6c5fb0bdf518b8e7
Yordy2001 <kingyordi2001@gmail.com>
2022-01-18 13:57:50 -0500

    add put route

---
+++ routers/Pokemons/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const autenticate = require('../../middleware/authenticate')
+
+const getAll = require('./headers/getAll')
+const getByName = require('./headers/getByName')
+const getById = require('./headers/getById')
+const addPokemon = require('./headers/addPokemon')
+const deletePokemon = require('./headers/deletePokemon')
+const updatePokemon = require('./headers/updatePokemon')
+
+const  router  = express.Router()
+
+router.get('/id/:id', getById)
+router.use('/name/:name', getByName)
+router.get('/',  getAll)
+
+router.post('/', addPokemon)
+router.delete('/', autenticate, deletePokemon)
+router.put('/', autenticate, updatePokemon)
+
+module.exports = router
