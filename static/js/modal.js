c5ad8e50d619824d8698d63faa67146bec43f456
Yordy2001 <kingyordi2001@gmail.com>
2022-02-02 13:45:48 -0500

    refactor

--- static/js/modal.js
+++ static/js/modal.js
@@ -3,28 +3,21 @@ const handleLogout = async ()=>{
   window.location.href = '/login'
 }   
 // Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
+const addBtn = document.getElementById("addBtn");
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
+
 
 // When the user clicks on the button, open the modal
-const openModal = (e)=>{
-  console.log(e.target)
-  
-  modal.style.display = "block";
-}
-
-btn.addEventListener('click', function(){
-  console.log("open")
+addBtn.addEventListener('click', function(){
   modal.style.display = "block";
 })
+
 // When the user clicks on <span> (x), close the modal
 span.addEventListener('click', function(){
-  console.log("close")
   modal.style.display = "none";
 })
 
