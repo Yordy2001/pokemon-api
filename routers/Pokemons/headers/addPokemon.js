355e19bcecd8aa33d963dbe0d5159bde4aafa7b9
Yordy2001 <kingyordi2001@gmail.com>
2022-02-17 21:26:49 -0400

    add multer depence

--- routers/Pokemons/headers/addPokemon.js
+++ routers/Pokemons/headers/addPokemon.js
@@ -1,21 +1,23 @@
 const { Pokemon } = require('../../../db')
 
 module.exports = async (req, res) =>{
-    const {name, img, description, owner, pokemonAbilityId, pokemonTypeId} = req.body
-    const UserId =  req.session.user.id
-    try {
-        // Get userId
-        await Pokemon.create({
-            name,
-            img,
-            description,
-            owner,
-            pokemonAbilityId,
-            pokemonTypeId,
-            UserId,
-        })
-    } catch (error) {
-        console.log(error)
-    }
-    res.redirect('/pokemon')
+    console.log(req.file, req.body)
+    // const {name, description, owner, pokemonAbilityId, pokemonTypeId} = req.body
+    // const { avatar } = req.file
+    // const UserId =  req.session.user.id
+    // try {
+    //     // Get userId
+    //     await Pokemon.create({
+    //         name,
+    //         avatar,
+    //         description,
+    //         owner,
+    //         pokemonAbilityId,
+    //         pokemonTypeId,
+    //         UserId,
+    //     })
+    // } catch (error) {
+    //     console.log(error)
+    // }
+    // res.redirect('/pokemon')
 }
--- routers/Pokemons/index.js
+++ routers/Pokemons/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const autenticate = require('../../middleware/authenticate')
+const multer  = require('multer')
+const upload = multer({ dest: 'static/images' })
 
 const getAll = require('./headers/getAll')
 const getByName = require('./headers/getByName')
@@ -11,10 +13,10 @@ const updatePokemon = require('./headers/updatePokemon')
 const  router  = express.Router()
 
 router.get('/id/:id', autenticate, getById)
-router.use('/name/:name', autenticate, getByName)
+router.get('/name/:name', autenticate, getByName)
 router.get('/', autenticate, getAll)
 
-router.post('/', autenticate, addPokemon)
+router.post('/', autenticate, upload.single('avatar'), addPokemon)
 router.delete('/id/:id', autenticate, deletePokemon)
 router.put('/id/:id', updatePokemon)
 
