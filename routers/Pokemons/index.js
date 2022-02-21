8f44400b1add6365a638b631b84ace3fa7f3b771
Yordy2001 <kingyordi2001@gmail.com>
2022-02-21 10:00:29 -0500

    fix multer

--- routers/Pokemons/index.js
+++ routers/Pokemons/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const autenticate = require('../../middleware/authenticate')
 const path = require('path')
-
 const multer  = require('multer')
+
 const storage = multer.diskStorage({ 
     destination: (req, file, cb) =>{
         cb( null, 'static/images')
